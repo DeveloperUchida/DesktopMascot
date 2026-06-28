0ead5cb2b0164b62b25ae9e65bfa3828d5892205
DeveloperUchida <104475269+DeveloperUchida@users.noreply.github.com>
2026-06-28 09:49:39 +0900

    InternalFirstCommit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DesktopMascot
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string[] messages =
+        {
+              "作業しててえらい〜！✨",
+            "水飲んだ？🚰",
+            "そろそろ休憩しよ〜☕",
+            "今日もちゃんと進んでるよ！",
+            "Gitコミット忘れてない？💻"
+        };
+        private readonly Random random = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MouseLeftButtonDown += (_, _) =>
+            {
+                MessageText.Text = messages[random.Next(messages.Length)];
+            };
+        }
+    }
+}
